9bc65f6b59bc76abf760c3f1bdbaaa5c269c2f3f
cybergas123 <103848773+cybergas123@users.noreply.github.com>
2023-11-28 21:10:18 +0100

    Add files via upload

---
+++ login.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    while ()
+    {
+        /* code */
+    }
+    
+    cout<<"Login please \nUsername : ";
+    //username
+    string username;
+    string password;
+    cin >> username;
+    //password
+    cout << "Password : ";
+    cin >> password;
+    if (username == "user" && password == "password") {
+        cout << "it works";
+    } else {
+        cout << "wrong password or username";
+    }
+
+    
+
+    return 0;
+    
+
+    
+
+}
